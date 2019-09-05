1ef9c443450dfdc78b5b6edec30b1e36866f8ba7
Wentao Shi <wentao771@gmail.com>
2019-09-05 15:56:08 -0500

    added XGBoost which can handle missing data

--- p_value_prediction/p_value_xgboost.py
+++ p_value_prediction/p_value_xgboost.py
@@ -3,11 +3,11 @@ Using resgression models to predict p-values according to the features of SNPs.
 '''
 import numpy as np
 import pandas as pd
+import xgboost as xgb
 from sklearn.model_selection import StratifiedKFold
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import roc_curve
-from sklearn.metrics import matthews_corrcoef, precision_score, recall_score
+from sklearn.metrics import accuracy_score, matthews_corrcoef, precision_score, recall_score
 from sklearn.metrics import f1_score, confusion_matrix
+from sklearn.metrics import roc_curve
 from statistics import mean 
 
 #------------------------------------------
@@ -38,7 +38,12 @@ df = df[['wildtype_value',
          'malacards_score', 
          'P-value', 
          'Gene_expression']] #selecting needed columns
-print(df)
+#print(df)
+
+# replace 0 in Gene_expression with np.nan because it represents missing value
+gene_expressions = df['Gene_expression']
+gene_expressions = [np.nan if g==0 else g for g in gene_expressions]
+df['Gene_expression'] = gene_expressions
 
 # add a column indicating the prediction result (0 or 1)
 p_values = df['P-value']
@@ -60,7 +65,7 @@ num_pos = df_pos.shape[0]
 num_neg = df_neg.shape[0]
 print('number of positive samples:', num_pos)
 print('number of negative samples:', num_neg)
-class_weight = {0:num_pos, 1:num_neg} 
+scale_pos_weight = float(num_neg/num_pos)
 
 # data and label
 X = np.array(df.drop(columns = ['classification_result']))
@@ -68,81 +73,63 @@ y = np.array(df['classification_result'])
 skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=123)
 
 #------------------------------------------
-#           Random Forest 
+#           XGBoost classifier
 #------------------------------------------
 # list contains dictionarys for each fold
 train_acc_records = []
 val_acc_records = []
 val_precision_records = []
 val_recall_records = []
+val_f1_records = []
 val_mcc_records = []
 fpr_records = []
 tpr_records = []
 thresholds_records = []
 
 # run the model in a cross-validation manner
+print('--------------------------------------------------')
 for train_index, val_index in skf.split(X, y):
-    rf = RandomForestClassifier(n_estimators=1000,
-                                max_depth=None,
-                                min_samples_split=5,
-                                random_state=0,
-                                max_features = "auto",
-                                criterion = "gini",
-                                class_weight = class_weight,
-                                n_jobs = -1)
-
+    print('Starting cross-validation on XGBoost model...')
+    xgb_clf = xgb.XGBClassifier(n_estimators=3000, verbosity=1, n_jobs=1, scale_pos_weight=scale_pos_weight, random_state=123)
     X_train, X_val = X[train_index], X[val_index]
     y_train, y_val = y[train_index], y[val_index]
-    rf.fit(X_train, y_train)# train the random forest
-    
-    # train the rf on the training data
-    print('training the random forest...')
-    rf.fit(X_train, y_train)
+    print('training XGBoost model...')
+    xgb_clf.fit(X_train, y_train)# train the random forest
     print('training finished.')
 
-    # training performance
-    train_acc = rf.score(X_train, y_train)
+    preds_train = xgb_clf.predict(X_train) # predictions on training data
+    preds_val = xgb_clf.predict(X_val) # predictions on validation data
+
+    # training accuracy
+    train_acc = accuracy_score(y_train, preds_train)
     train_acc_records.append(train_acc)
-    print('training accuracy:', train_acc)
-    
-    # validation performance
-    predictions = rf.predict(X_val)
-    num_correct = np.sum(predictions == y_val)
-    print('number of correct predictions:',num_correct)
-    
-    val_acc = rf.score(X_val, y_val)
+    print('training accuracy: ', train_acc)
+
+    # validation accuracy
+    val_acc = accuracy_score(y_val, preds_val)
     val_acc_records.append(val_acc)
-    print('validation accuracy:', val_acc)
-    
-    val_precision = precision_score(y_val,predictions)
-    val_precision_records.append(val_precision)        
+    print('validation accuracy: ', val_acc)
+
+    # validation precision
+    val_precision = precision_score(y_val, preds_val)
+    val_precision_records.append(val_precision)
     print('validation precision:', val_precision)
-    
-    val_recall = recall_score(y_val,predictions)
-    val_recall_records.append(val_recall)        
-    print('validation recall:', val_recall)
-    
-    val_mcc = matthews_corrcoef(y_val, predictions)
-    val_mcc_records.append(val_mcc)
-    print('validation mcc:', val_mcc)
-    
-    val_f1 = f1_score(y_val, predictions)
+
+    # validation recall
+    val_recall = recall_score(y_val, preds_val)
+    val_recall_records.append(val_recall)
+    print('validation recall: ', val_recall)
+
+    # validation F1
+    val_f1 = f1_score(y_val, preds_val)
+    val_f1_records.append(val_f1)
     print('validation f1:', val_f1)
-    
-    val_cm = confusion_matrix(y_val, predictions)
-    print('validation confusion matrix:', val_cm)
-    
-    # output probabilities for val data
-    val_prob = rf.predict_proba(X_val)
-    fpr, tpr, thresholds = roc_curve(y_val, val_prob[:, 1])
-    
-    # convert to list so that can be saved in .json file
-    fpr = fpr.tolist()
-    tpr = tpr.tolist()
-    thresholds = thresholds.tolist()
-    fpr_records.append(fpr)
-    tpr_records.append(tpr)
-    thresholds_records.append(thresholds)
+
+    # validation MCC
+    val_mcc = matthews_corrcoef(y_val, preds_val)
+    val_mcc_records.append(val_mcc)
+    print('validation MCC:', val_mcc)
+
     print('--------------------------------------------------')
 
 #------------------------------------------
@@ -153,6 +140,7 @@ print('averaged training accuracy:', mean(train_acc_records))
 print('averaged validation accuracy:', mean(val_acc_records))
 print('averaged validation precision:', mean(val_precision_records))
 print('averaged validation recall:', mean(val_recall_records))
+print('averaged validation F1:', mean(val_f1_records))
 print('averaged validation MCC:', mean(val_mcc_records))
 
 # rank the features
